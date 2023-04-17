0b9c7c791368def1d7326f3447cf5d62aa32fca7
HajraZawwar <hajrazawwar@gmail.com>
2023-04-17 15:02:22 +0500

    Third commit

--- app/src/main/java/com/hajra/salarycalculator/MainActivity.java
+++ app/src/main/java/com/hajra/salarycalculator/MainActivity.java
@@ -2,12 +2,32 @@ package com.hajra.salarycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ed1,ed2,ed3,ed4;
+    Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ed1=findViewById(R.id.empname);
+        ed2=findViewById(R.id.empsal);
+        ed3=findViewById(R.id.emptax);
+        ed1=findViewById(R.id.salary);
+
+        btn1=findViewById(R.id.btn1);
+
+
+
+
+
+
+
     }
 }
