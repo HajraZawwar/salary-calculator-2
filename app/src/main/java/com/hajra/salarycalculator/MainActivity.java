8cd409a3a10e4c11aab47fa913855104ec1c5eb6
HajraZawwar <hajrazawwar@gmail.com>
2023-04-17 15:56:00 +0500

    First commit

--- app/src/main/java/com/hajra/salarycalculator/MainActivity.java
+++ app/src/main/java/com/hajra/salarycalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.hajra.salarycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,13 +21,43 @@ public class MainActivity extends AppCompatActivity {
         ed2=findViewById(R.id.empsal);
         ed3=findViewById(R.id.emptax);
         ed1=findViewById(R.id.salary);
+        btn1=findViewById(R.id.btn1);
 
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                empsal();
+            }
 
+            }
+        });
 
+        public void empsal()
+        {
 
+            double salary = Double.parseDouble(ed2.getText().toString());
+            double tax, nsal;
 
+            if (salary> 500000)
+            {
+                tax= salary * 10/100;
+            }
 
-    }
-}
+            else if(salary > 1000000 )
+            {
+                tax= salary * 20/100;
+            }
+
+            else
+            {  tax= 0;
+
+            }
+
+            ed3.setText(String.valueOf(tax));
+
+            nsal= salary-tax;
+
+            ed4.setText(String.valueOf(nsal));
+        }
